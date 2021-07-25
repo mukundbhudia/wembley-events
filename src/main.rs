eee849ba635cf396b6b61e87413a0d9f59e0f64e
Mukund Bhudia <mukund_bhudia@hotmail.com>
2021-07-25 16:57:28 +0100

    feat: wired up basic fetch & scrape

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,18 @@
-fn main() {
-    println!("Hello, world!");
+extern crate reqwest;
+extern crate select;
+
+use select::document::Document;
+use select::predicate::Name;
+
+#[tokio::main]
+async fn main() -> Result<(), reqwest::Error> {
+    let url = "https://www.brent.gov.uk/events-and-whats-on-calendar/?eventCat=Wembley+Stadium+events&startDate=01%2F01%2F2021&endDate=31%2F12%2F2029&count=50";
+
+    let res = reqwest::get(url).await?;
+    let body = res.text().await?;
+
+    Document::from(body.as_str())
+        .find(Name("h3"))
+        .for_each(|x| println!("{:?}", x.text()));
+    Ok(())
 }
