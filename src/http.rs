84b7ec0543e3eb87cfa25712f84b7c29bf8d0573
Mukund Bhudia <mukund_bhudia@hotmail.com>
2021-09-05 10:23:17 +0100

    test: added basic http access test

--- src/http.rs
+++ src/http.rs
@@ -1,5 +1,16 @@
 use std::error::Error;
 
+#[derive(Debug)]
+pub struct HttpResponse {
+    pub status: u16,
+    pub body: String,
+}
+
+impl HttpResponse {
+    pub fn new(status: u16, body: String) -> Self {
+        Self { status, body }
+    }
+}
 pub struct HttpClient {
     url: String,
 }
@@ -8,10 +19,22 @@ impl HttpClient {
     pub fn new(url: &str) -> Self {
         HttpClient { url: url.into() }
     }
-    pub async fn get_text_from_url(&self) -> Result<String, Box<dyn Error>> {
+    pub async fn get_text_from_url(&self) -> Result<HttpResponse, Box<dyn Error>> {
         let res = reqwest::get(&self.url).await?;
-        Ok(res.text().await?)
+        Ok(HttpResponse::new(res.status().as_u16(), res.text().await?))
     }
 }
 
-// TODO: consider async http client test for a basic URL (e.g.: https://httpbin.org/get)
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[tokio::test]
+    async fn basic_http_access_test() {
+        let http_response = HttpClient::new("https://httpbin.org/get")
+            .get_text_from_url()
+            .await
+            .unwrap();
+        assert_eq!(200, http_response.status);
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -6,7 +6,7 @@ use wembley_events::{HttpClient, WembleyEvents};
 async fn main() -> Result<(), Box<dyn Error>> {
     let body = HttpClient::new("https://www.brent.gov.uk/events-and-whats-on-calendar/?eventCat=Wembley+Stadium+events&startDate=01%2F01%2F2021&endDate=31%2F12%2F2029&count=50")
         .get_text_from_url()
-        .await?;
+        .await?.body;
     let wembley_events = WembleyEvents::new().build_calendar_from_html(body);
 
     println!("{:#?}", &wembley_events.get_events());
