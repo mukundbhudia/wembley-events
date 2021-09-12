bbe34afd748dc67e914430d088acd67a658cd6f2
mukundbhudia <mukundbhudia@users.noreply.github.com>
2021-09-12 19:35:29 +0000

    Deploying to gh-pages from @ mukundbhudia/wembley-events@1da88166a3142ee721fc84b5cd9901bf5ee88674 🚀

--- README.md
+++
@@ -1,3 +0,0 @@
-# wembley-events
-
-Generates an iCal file on all the upcoming events at Wembley Stadium
--- src/lib.rs
+++
@@ -1,8 +0,0 @@
-mod events;
-mod http;
-mod test_files;
-
-pub use events::WembleyEvent;
-pub use events::WembleyEvents;
-pub use http::HttpClient;
-pub use test_files::{test_file_1, test_file_2};
