915edb1c52cb796b9375be9d18e7885570247ec9
Mukund Bhudia <mukund_bhudia@hotmail.com>
2021-09-20 08:20:05 +0100

    feat: improved directory handling on file creation

--- src/calendar_writer.rs
+++ src/calendar_writer.rs
@@ -11,17 +11,24 @@ impl CalendarWriter {
     }
 
     pub fn write_calendar_to_file(self, path: &str) -> Result<(), CalendarWriterError> {
-        if self.calendar.is_empty() {
-            eprintln!("Unable to write calendar to file. Calendar has no events. Check if the HTML structure has changed.");
-            Err(CalendarWriterError::EmptyCalendar)
-        } else {
-            match fs::write(path, self.calendar.to_string()) {
-                Ok(_) => Ok(()),
-                Err(e) => {
-                    eprintln!("Unable to write calendar to file. {:#?}", e);
-                    Err(CalendarWriterError::CannotWriteToFile)
-                }
+        let path = std::path::Path::new(path);
+
+        if let Some(prefix) = path.parent() {
+            if self.calendar.is_empty() {
+                eprintln!("Unable to write calendar to file. Calendar has no events. Check if the HTML structure has changed.");
+                Err(CalendarWriterError::EmptyCalendar)
+            } else if let Err(e) = fs::create_dir_all(prefix) {
+                eprintln!("Unable to directory for calendar. {:#?}", e);
+                Err(CalendarWriterError::CannotCreateDirectory)
+            } else if let Err(e) = fs::write(path, self.calendar.to_string()) {
+                eprintln!("Unable to write calendar to file. {:#?}", e);
+                Err(CalendarWriterError::CannotWriteToFile)
+            } else {
+                Ok(())
             }
+        } else {
+            eprintln!("Unable to write calendar to file. Bad path given.");
+            Err(CalendarWriterError::BadFilePath)
         }
     }
 }
@@ -30,6 +37,8 @@ impl CalendarWriter {
 pub enum CalendarWriterError {
     EmptyCalendar,
     CannotWriteToFile,
+    CannotCreateDirectory,
+    BadFilePath,
 }
 
 #[cfg(test)]
@@ -42,7 +51,6 @@ mod tests {
     fn test_calendar_writer_empty_calendar_to_write() {
         let calendar = Calendar::new();
 
-        fs::create_dir_all("output/").expect("Unable to create output directory for testing");
         let writer = CalendarWriter::new(calendar).write_calendar_to_file("output/test_fail.ics");
 
         assert_eq!(writer, Err(CalendarWriterError::EmptyCalendar));
@@ -55,12 +63,10 @@ mod tests {
             .build_events_from_html(body)
             .build_calendar_from_events();
 
-        fs::create_dir_all("output/").expect("Unable to create output directory for testing");
-
         let writer =
             CalendarWriter::new(wembley_events).write_calendar_to_file("/output/test_fail.ics");
 
-        assert_eq!(writer, Err(CalendarWriterError::CannotWriteToFile));
+        assert_eq!(writer, Err(CalendarWriterError::CannotCreateDirectory));
     }
 
     #[test]
@@ -69,8 +75,6 @@ mod tests {
         let wembley_events = WembleyEvents::new()
             .build_events_from_html(body)
             .build_calendar_from_events();
-
-        fs::create_dir_all("output/").expect("Unable to create output directory for testing");
 
         let writer =
             CalendarWriter::new(wembley_events).write_calendar_to_file("/output/test_fail.ics");
@@ -85,10 +89,21 @@ mod tests {
             .build_events_from_html(body)
             .build_calendar_from_events();
 
-        fs::create_dir_all("output/").expect("Unable to create output directory for testing");
-
         let writer =
             CalendarWriter::new(wembley_events).write_calendar_to_file("output/test_run.ics");
+
+        assert_eq!(writer, Ok(()));
+    }
+
+    #[test]
+    fn test_calendar_writer_nested_directory() {
+        let body = test_file_1();
+        let wembley_events = WembleyEvents::new()
+            .build_events_from_html(body)
+            .build_calendar_from_events();
+
+        let writer = CalendarWriter::new(wembley_events)
+            .write_calendar_to_file("output/test1/test2/test_run.ics");
 
         assert_eq!(writer, Ok(()));
     }
--- src/main.rs
+++ src/main.rs
@@ -5,6 +5,7 @@ use wembley_events::{CalendarWriter, HttpClient, WembleyEvents};
 #[tokio::main]
 async fn main() {
     let calendar_url = "https://www.brent.gov.uk/events-and-whats-on-calendar/?eventCat=Wembley+Stadium+events&startDate=01%2F01%2F2021&endDate=31%2F12%2F2029&count=50";
+    let calendar_save_path = "output/wembley-events.ics";
 
     if let Ok(res) = HttpClient::new(calendar_url).get_text_from_url().await {
         let wembley_events_calendar = WembleyEvents::new()
@@ -12,7 +13,7 @@ async fn main() {
             .build_calendar_from_events();
 
         if CalendarWriter::new(wembley_events_calendar)
-            .write_calendar_to_file("output/wembley-events.ics")
+            .write_calendar_to_file(calendar_save_path)
             .is_err()
         {
             process::exit(1);
